3edeed58890e91826cdd67f83d97fd3b1074ad4b
Mikko Hellsing <mikko@aino.se>
2015-11-17 22:03:07 +0100

    change run behaviour

--- template/ghetto.js
+++ template/ghetto.js
@@ -1,9 +1,9 @@
 'use strict'
 
 
+require('babel-core/register')
+
+
 let Ghetto = require('ghetto').Ghetto
 let ghetto = module.exports = new Ghetto(__dirname)
 // customize your ghetto instance here if needed
-
-
-ghetto.run()
